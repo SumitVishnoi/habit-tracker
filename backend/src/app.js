60e046fd329058d69bff5a3745d37f9395118bbb
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-08-27 22:03:23 +0530

    style: format code for consistency and readability

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,25 @@
-import express from "express"
+import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 import prisma from "./utils/prisma.js";
 import authRouter from "./routes/auth.routes.js";
 import habitRouter from "./routes/habits.routes.js";
 import checkinRouter from "./routes/checkin.routes.js";
+import path from "path";
 
-
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+
 app.use(cors({
-    origin: ["http://localhost:5173", "https://habitly-w7j8.onrender.com"],
-    credentials: true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://habitly-w7j8.onrender.com"
+  ],
+  credentials: true
+}));
+
 app.use(express.static(path.join(process.cwd(), "public")));
 
 app.get("/", async (req, res) => {
@@ -35,9 +40,13 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.use("/api/auth", authRouter)
-app.use("/api/habits", habitRouter)
-app.use("/api", checkinRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/habits", habitRouter);
+app.use("/api", checkinRouter);
 
+// Wildcard route — must be AFTER API routes
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
 
 export default app;
